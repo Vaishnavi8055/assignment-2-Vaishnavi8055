cb666461b6c82672d3bcc9b2c11297abce8ae2bd
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 09:07:03 +1400

    private variables declared

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,15 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Vaishnavi
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+
 }
