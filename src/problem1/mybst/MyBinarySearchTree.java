1496d856a086d414f050f246fecf21fbaedcccd8
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 09:11:55 +1400

    static declared

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,10 +1,18 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Vaishnavi
  *  Date: 23-Mar-20
  *  Time: 7:17 PM
  */
 package problem1.mybst;
 // to implement BinarySearchTree
+
+import problem1.node.TreeNode;
+
+import java.util.Queue;
+
 public class MyBinarySearchTree {
+static Queue<TreeNode> queue;
+
+
 }
