31bb38e91eb77c9aee00f7ffcbbaaf24b4b00a14
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 23:11:43 +1400

    Main Class created

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -8,12 +8,28 @@ package problem5.main;
 
 import problem5.circularqueue.MyCircularQueue;
 import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
 
 //executable class
 public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue myCircularQueue =new MyCircularQueue();
         Node node;
+
+        for(int k=0;k<5;k++){
+            node=new Node (new Student());
+            myCircularQueue.enqueueNode(node);
+        }
+
+        myCircularQueue.displayQueue();
+
+        myCircularQueue.remove(new Scanner(System.in).next());
+        myCircularQueue.displayQueue();
+
+        myCircularQueue.processing(new Scanner(System.in).next());
+
 
     }
 }
