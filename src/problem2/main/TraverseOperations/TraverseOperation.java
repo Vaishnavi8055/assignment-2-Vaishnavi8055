7b5cc496aff2e275b55b0b785cd3be88f6f60e29
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 21:43:34 +1400

    postOrderTraverse method

---
+++ src/problem2/main/TraverseOperations/TraverseOperation.java
@@ -0,0 +1,11 @@
+package problem2.main.TraverseOperations;
+
+import problem1.node.TreeNode;
+
+public class TraverseOperation {
+
+    public void displayPostOrderTraverse(TreeNode node){
+
+    }
+
+}
