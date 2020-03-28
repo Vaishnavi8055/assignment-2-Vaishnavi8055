36e64ccb6868be64cc3347116304aed6e1bdf5c6
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 19:42:21 +1400

    MyBST class Created

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -15,27 +15,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class MyBinarySearchTree {
-   static Queue<TreeNode> queue;
+    static Queue<TreeNode> queue;
 
-   public TreeNode insertNode(TreeNode root,int data) {
-       if (root == null) {
-           return new TreeNode(data);
-       } else if (data > root.getData()) {
-           root.setRight(insertNode(root.getRight(), data));
-       } else {
-           root.setLeft(insertNode(root.getLeft(), data));
-       }
-       return root;
-   }
+    public TreeNode insertNode(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        } else if (data > root.getData()) {
+            root.setRight(insertNode(root.getRight(), data));
+        } else {
+            root.setLeft(insertNode(root.getLeft(), data));
+        }
+        return root;
+    }
 
-   public void countNodesWithoutLeftChild(TreeNode root){
-       if(root==null){
-           return;
-       }
-       Queue<TreeNode> queues=new LinkedList<TreeNode>();
+    public void countNodesWithoutLeftChild(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queues = new LinkedList<TreeNode>();
         queues.add(root);
-        int count=0;
-        while(queues !=null) {
+        int count = 0;
+        while (queues != null) {
             TreeNode current = queues.poll();
             if (current == null) {
                 break;
@@ -50,24 +50,31 @@ public class MyBinarySearchTree {
                 queues.add(current.getRight());
             }
         }
-            System.out.println("Number of Nodes Without Left Child is :--"+count);
-   }
-
-   public void printLeftChildren(TreeNode root){
-    if(root==null){
-    return;
+        System.out.println("Number of Nodes Without Left Child is :--" + count);
     }
-    queue.add(root);
-    queue.add(null);
-    while(queue.size()>0){
-     TreeNode current=queue.peek();
-     if(current!=null){
-         System.out.println(current.getData()+" , ");
-     }
-     while(queue.peek()!=null){
 
-     }
+    public void printLeftChildren(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        queue.add(root);
+        queue.add(null);
+        while (queue.size() > 0) {
+            TreeNode current = queue.peek();
+            if (current != null) {
+                System.out.println(current.getData() + " , ");
+                while (queue.peek() != null) {
+                    if (current.getLeft() != null) {
+                        queue.add(current.getLeft());
+                    }
+                    if (current.getRight() != null)
+                        queue.add(current.getRight());
+                    queue.remove();
+                    current = queue.peek();
+                }
+                queue.add(null);
+            }
+            queue.remove();
+        }
     }
-   }
-
 }
