b9184021fe9d75557806816ac7c9d306a079a5c6
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 20:44:47 +1400

    private front of queue declared

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,17 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Vaishnavi
  *  Date: 23-Mar-20
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+
+import problem1.node.TreeNode;
+import problem4.Node.Node;
+
 public class MyQueue {
+   private TreeNode treeNode;
+   private Node front;
 }
