d2216329e2220bbc29487eaf04cf228504a09f5a
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 22:13:51 +1400

    private declared scanner object

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,10 +5,14 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
+import java.util.Scanner;
+
 // to store student information and properties
 public class Student {
     private String studentName;
     private int studentUnivRollNumber;
     private int studentTotalBackLogs;
     private int studentApperingCount;
+    private Scanner scanner;
 }
