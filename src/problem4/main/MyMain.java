58f51b6e5038b5f65c7b3ac1c837f9a32ca2fdd2
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 21:11:55 +1400

    MyBinarySearchTree obj created

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,19 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Vaishnavi
  *  Date: 23-Mar-20
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    Scanner sc=new Scanner(System.in);
+    MyBinarySearchTree myBinarySearchTree=new MyBinarySearchTree();
+
 }
