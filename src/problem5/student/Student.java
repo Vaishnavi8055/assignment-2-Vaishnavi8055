7b80b0c16591e909d2d798781e042624fb55854f
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 22:19:13 +1400

    Constructor of student class created

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -15,4 +15,19 @@ public class Student {
     private int studentTotalBackLogs;
     private int studentApperingCount;
     private Scanner scanner;
+
+    public Student() {
+        scanner = new Scanner(System.in);
+        System.out.println("Name of Student : ");
+        this.studentName = scanner.next();
+        System.out.println("University RollNumber of Student : ");
+        this.studentUnivRollNumber = scanner.nextInt();
+        System.out.println("Number Of Backlogs Of a Student : ");
+        this.studentTotalBackLogs = scanner.nextInt();
+        System.out.println("Appering Count Of Student : ");
+        this.studentApperingCount = scanner.nextInt();
+    }
+}
+
+    }
 }
