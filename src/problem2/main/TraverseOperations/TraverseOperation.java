1bf5207d0677cc1a19958fa7f47904c3ed65a830
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 21:51:42 +1400

    TraverseOperations class created

--- src/problem2/main/TraverseOperations/TraverseOperation.java
+++ src/problem2/main/TraverseOperations/TraverseOperation.java
@@ -4,22 +4,30 @@ import problem1.node.TreeNode;
 
 public class TraverseOperation {
 
-    public void displayPostOrderTraverse(TreeNode node){
-       if(node==null) {
-           return;
-       }
-           displayPostOrderTraverse(node.getLeft());
-           displayPostOrderTraverse(node.getRight());
-           System.out.println(node.getData()+" ");
+    public void displayPostOrderTraverse(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        displayPostOrderTraverse(node.getLeft());
+        displayPostOrderTraverse(node.getRight());
+        System.out.println(node.getData() + " ");
     }
 
-    public void displayPreOrderTraverse(TreeNode node){
-       if(node==null){
-           return;
-       }
-        System.out.println(node.getData()+" ");
+    public void displayPreOrderTraverse(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.getData() + " ");
         displayPreOrderTraverse(node.getLeft());
         displayPreOrderTraverse(node.getRight());
     }
 
+    public void displayInOrderTraverse(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        displayInOrderTraverse(node.getLeft());
+        System.out.println(node.getData() + " ");
+        displayInOrderTraverse(node.getRight());
+    }
 }
