4f6a43902562cf4e75c90cd58a992250ba98fccc
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2020-03-28 22:30:15 +1400

    Front Node is declared private

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -1,10 +1,15 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Vaishnavi
  *  Date: 23-Mar-20
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node frontNode;
+
 }
